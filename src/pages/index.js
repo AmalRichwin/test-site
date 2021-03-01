a8052df4185498fef357d5297287848b5dbfd8f9
AmalRichwin-AT <amal_r_amogha_tech@outlook.com>
2021-03-01 10:00:35 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,37 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <form
+      name="contactusForm"
+      method="POST"
+      action="/contact-thanks"
+      data-netlify-honeypot="bot-field"
+      data-netlify="true"
+      id="contact-form"
+      className="contact-form"
+    >
+      <input name="bot-field" hidden />
+      <input
+        type="text"
+        name="name"
+        id="contact-form-name"
+        className="form-input"
+      />
+      <input
+        type="email"
+        name="email"
+        id="contant-form-email"
+        className="form-input"
+      />
+      <textarea
+        name="message"
+        id="contant-form-message"
+        className="form-textarea"
+        rows="7"
+      />
+      <input type="hidden" name="form-name" value="contactusForm" />
+      <button type="submit">Send Message</button>
+    </form>
   </Layout>
 )
 

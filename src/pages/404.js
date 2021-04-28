deba34777569dff8cd30148d1739d8bc7394607d
Richwin_ <77601299+AmalRichwin-AT@users.noreply.github.com>
2021-04-28 23:50:06 +0530

    changed the layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,29 @@
-import React from "react"
+import * as React from 'react'
+import styled from 'styled-components'
+import GifImage from '../images/animation_200_ko1qponm.gif'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from '../components/Seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const Container = styled.div`
+	margin: 3rem auto;
+	max-width: 600px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 80vh;
+`
+
+// markup
+const NotFoundPage = () => {
+	return (
+		<>
+			<Seo title="404" />
+			<Container>
+				<img src={GifImage} alt="404" />
+			</Container>
+		</>
+	)
+}
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,50 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import styled from 'styled-components'
+import PulseLoader from 'react-spinners/PulseLoader'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Seo from '../components/Seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    <form
-      name="contactusForm"
-      method="POST"
-      action="/contact-thanks"
-      data-netlify-honeypot="bot-field"
-      data-netlify="true"
-      id="contact-form"
-      className="contact-form"
-    >
-      <input name="bot-field" hidden />
-      <input
-        type="text"
-        name="name"
-        id="contact-form-name"
-        className="form-input"
-      />
-      <input
-        type="email"
-        name="email"
-        id="contant-form-email"
-        className="form-input"
-      />
-      <textarea
-        name="message"
-        id="contant-form-message"
-        className="form-textarea"
-        rows="7"
-      />
-      <input type="hidden" name="form-name" value="contactusForm" />
-      <button type="submit">Send Message</button>
-    </form>
-  </Layout>
-)
+const Container = styled.div`
+	margin: 3rem auto;
+	max-width: 600px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 60vh;
+`
+const H1 = styled.h1`
+	font-family: 'Cinzel Decorative', cursive;
+	font-size: 3.5rem;
+	color: #e6e6e6;
+`
+const P1 = styled.p`
+	font-family: 'Sacramento', cursive;
+	font-size: 2rem;
+	color: #e6e6e6;
+`
+const P2 = styled.p`
+	font-family: 'Sacramento', cursive;
+	font-size: 2rem;
+	color: #e6e6e6;
+`
+
+const IndexPage = () => {
+	return (
+		<>
+			<Seo title="Amal Richwin" />
+			<Container>
+				<H1>Amal Richwin</H1>
+				<div>
+					<P1>About a passionate dev</P1>
+					<P2>
+						Soon
+						<PulseLoader color="#e6e6e6" loading={true} size={5} />
+					</P2>
+				</div>
+			</Container>
+		</>
+	)
+}
 
 export default IndexPage

3252f0b89d9191fc3735f9857724b3a2e6143fa2
Richwin_ <77601299+AmalRichwin-AT@users.noreply.github.com>
2021-05-29 10:59:46 +0530

    made changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import useSound from 'use-sound'
 
 import Song from '../images/song.mp3'
+import illus from '../images/wedding_illus.svg'
 
 const Container = styled.div`
 	margin: 5rem auto;
@@ -49,30 +50,46 @@ const P = styled.p`
 	text-align: center;
 	color: #303030;
 `
-// const P1 = styled.p`
-// 	font-family: 'Sacramento', cursive;
-// 	font-size: 2rem;
-// 	color: #e6e6e6;
-// `
-// const P2 = styled.p`
-// 	font-family: 'Sacramento', cursive;
-// 	font-size: 2rem;
-// 	color: #e6e6e6;
-// `
 const Button = styled.p`
 	display: hidden;
 `
+const Anchor = styled.a`
+	color: black;
+`
+
 const Div = styled.div`
-	display: flex;
-	flex-direction: column;
+	box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, 0.2);
+	font-family: 'Dancing Script', cursive;
+	width: 18rem;
+	background: whitesmoke;
+	border-radius: 10px;
+	margin-top: 10%;
+	margin-bottom: 10%;
 	justify-content: center;
 	align-items: center;
-	background: whitesmoke;
-	font-family: 'Dancing Script', cursive;
-	height: 50rem;
-	width: 18rem;
-	border-radius: 10px;
-	box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, 0.2);
+	height: 30rem;
+	&::after {
+		content: '';
+		background: linear-gradient(45deg, #ffc691 48.9%, #ffc371 49%);
+		background-size: 300% 300%;
+		border-radius: 10px;
+		position: absolute;
+		top: -5px;
+		left: -5px;
+		height: calc(100% + 8px);
+		width: calc(100% + 8px);
+		z-index: -1;
+		animation: borderGradient 8s linear both infinite;
+	}
+	@keyframes borderGradient {
+		0%,
+		100% {
+			background-position: 0% 100%;
+		}
+		50% {
+			background-position: 100% 0%;
+		}
+	}
 	@media (min-width: 768px) {
 		height: 90rem;
 		width: 25rem;
@@ -80,7 +97,7 @@ const Div = styled.div`
 `
 
 const IndexPage = () => {
-	const [play, exposedData] = useSound(Song, {})
+	const [play] = useSound(Song)
 	React.useEffect(() => {
 		const btn = document.getElementById('play')
 		setTimeout(() => {
@@ -101,14 +118,6 @@ const IndexPage = () => {
 					</P2>
 				</div> */}
 				<Div>
-					{/* <h1>PLEASE JOIN US FOR THE 25TH ANNIVERSARY OF OUR PARENTS</h1>
-					<h2>KEEVIN</h2>
-					<p>with</p>
-					<h2>SHANTHI</h2>
-					<p>May 29th SATURDAY 2021</p>
-					<p>AT 5.30pm</p>
-					<p>Virtual Venue</p>
-					<p>Zoom</p> */}
 					<H1>PLEASE JOIN US FOR THE 25TH ANNIVERSARY OF OUR PARENTS</H1>
 					<div>
 						<H2>KEEVIN</H2>
@@ -117,13 +126,18 @@ const IndexPage = () => {
 					</div>
 					<P>May 29th SATURDAY 2021</P>
 					<P>AT 5.30pm</P>
-					<P2>Virtual Venue : Zoom</P2>
+					<P>Celebration of hearts through</P>
+					<P2>
+						Virtual Venue :{' '}
+						<Anchor href="https://us04web.zoom.us/j/76805254952?pwd=YmNhbmlSYVNtQlJyckJycGhoU2NiUT09">
+							Zoom
+						</Anchor>{' '}
+					</P2>
 					<Button
 						id="play"
 						hidden
 						onClick={() => {
 							play()
-							exposedData.sound.fade(0, 1, 100000)
 						}}
 					>
 						Play

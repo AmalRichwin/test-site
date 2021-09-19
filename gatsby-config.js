233a1544cdb66eae9e2170df024e26a416ab5c98
Richwin_ <77601299+AmalRichwin-AT@users.noreply.github.com>
2021-09-19 11:59:18 +0530

    feat: add typewriter effect
    feat: add schema for seo
    
    chore: updated all packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,42 @@
 module.exports = {
-	siteMetadata: {
-		title: 'Richie',
-		titleTemplate: 'Amal Richwin | Richie',
-		siteUrl: `https://amalrichwin.xyz/`,
-		description: `All about a passionate dev`,
-	},
-	plugins: [
-		'gatsby-plugin-styled-components',
-		'gatsby-plugin-image',
-		// {
-		//   resolve: "gatsby-plugin-google-analytics",
-		//   options: {
-		//     trackingId: "",
-		//   },
-		// },
-		'gatsby-plugin-react-helmet',
-		`gatsby-plugin-offline`,
-		'gatsby-plugin-sitemap',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: `Amal Richwin`,
-				short_name: `Richie`,
-				start_url: `/`,
-				background_color: `#0d0d0d`,
-				theme_color: `#0d0d0d`,
-				icon: 'src/images/favicon-32x32.png',
-			},
-		},
-		'gatsby-plugin-sharp',
-		'gatsby-transformer-sharp',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'images',
-				path: './src/images/',
-			},
-			__key: 'images',
-		},
-	],
-}
+  siteMetadata: {
+    title: 'Richie',
+    titleTemplate: 'Amal Richwin | Richie',
+    siteUrl: `https://amalrichwin.xyz/`,
+    description: `All about a passionate dev`,
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'G-F5Z9LM577Q',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-offline`,
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `Amal Richwin`,
+        short_name: `Richie`,
+        start_url: `/`,
+        background_color: `#0d0d0d`,
+        theme_color: `#0d0d0d`,
+        icon: 'src/images/favicon-32x32.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,140 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import { Helmet } from 'react-helmet'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+import TypeWriter from 'typewriter-effect';
 
 const Container = styled.div`
-	margin: 5rem auto;
-	max-width: 600px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	max-height: 60vh;
-`
+  margin: 5rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-height: 60vh;
+`;
 const H1 = styled.h1`
-	font-family: 'Anonymous Pro', monospace;
-	font-size: 1.25rem;
-	color: #505050;
-	text-align: center;
-`
+  font-family: 'Anonymous Pro', monospace;
+  font-size: 1.25rem;
+  color: #505050;
+  text-align: center;
+`;
 
 const IndexPage = () => {
-	return (
-		<>
-			<Seo title="Amal Richwin" />
-			<Container>
-				<H1>Amal Richwin</H1>
-			</Container>
-		</>
-	)
-}
+  // React.useEffect(() => {
+  // 	const script = document.createElement('script')
+  // 	script.type = 'text/javascript'
+  // 	script.src = 'https://dedicated-pioneer-5995.ck.page/f94c49936a/index.js'
+  // 	script.async = true
+  // 	script.dataUid = 'f94c49936a'
+  // 	document.appendChild(script)
+  // }, [])
+  return (
+    <>
+      <Seo title="Amal Richwin" />
+      <Container>
+        <H1>Amal Richwin</H1>
+        <TypeWriter
+          options
+          onInit={(typewriter) => {
+            typewriter.typeString('Portfolio').start();
+          }}
+        />
+      </Container>
+    </>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 const Seo = ({ description, lang, meta, title }) => {
-	const { site } = useStaticQuery(
-		graphql`
-			query {
-				site {
-					siteMetadata {
-						title
-						description
-					}
-				}
-			}
-		`
-	)
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
 
-	const metaDescription = description || site.siteMetadata.description
-	const defaultTitle = site.siteMetadata?.title
+  const metaDescription = description || site.siteMetadata.description;
+  const defaultTitle = site.siteMetadata?.title;
 
-	return (
-		<Helmet
-			htmlAttributes={{
-				lang,
-			}}
-			title={title}
-			titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-			meta={[
-				{
-					name: `description`,
-					content: `All about a passionate dev`,
-				},
-				{
-					property: `og:title`,
-					content: title,
-				},
-				{
-					property: `og:description`,
-					content: metaDescription,
-				},
-				{
-					property: `og:type`,
-					content: `website`,
-				},
-				{
-					name: `twitter:card`,
-					content: `summary`,
-				},
-				{
-					name: `twitter:creator`,
-					content: ``,
-				},
-				{
-					name: `twitter:title`,
-					content: title,
-				},
-				{
-					name: `twitter:description`,
-					content: metaDescription,
-				},
-			].concat(meta)}
-		/>
-	)
-}
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: `All about a passionate dev`,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: ``,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}>
+      <script type="application/Id+json">
+        {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Person",
+          "url": "https://www.spookytech.com",
+          "name": "Amal Richwin K",
+		  "description": "Amal richwin portfolio",
+          "contactPoint": {
+            "@type": "ContactPoint",
+            "telephone": "+5-601-785-8543",
+            "contactType": "Customer Support"
+          }
+        }
+      `}
+      </script>
+    </Helmet>
+  );
+};
 
 Seo.defaultProps = {
-	lang: `en`,
-	meta: [],
-	description: ``,
-}
+  lang: `en`,
+  meta: [],
+  description: ``,
+};
 
 Seo.propTypes = {
-	description: PropTypes.string,
-	lang: PropTypes.string,
-	meta: PropTypes.arrayOf(PropTypes.object),
-	title: PropTypes.string.isRequired,
-}
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
